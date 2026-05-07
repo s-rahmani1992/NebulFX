f46fd9c94b74862c2bdd1a63eb94aa28ec03df00
Sina Rahmani <rahmani.sina1992@gmail.com>
2026-05-07 11:09:59 +0100

    Added tailwindcss package for making the styling easier.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import './index.css'
 
 function App() {
 
   return (
     <>
-      
+      <div>
+        <h1 className="text-3xl font-extrabold pt-20">Vite + React</h1>
+      </div>
     </>
   )
 }
